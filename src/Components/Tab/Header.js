c0e1b39f0b38b5eae8fe19a11f84296321254142
Hassan97 <nguyenddse62732@gmail.com>
2022-08-26 16:48:43 +0700

    commit update

--- src/Components/Tab/Header.js
+++ src/Components/Tab/Header.js
@@ -1,17 +1,20 @@
-import React from 'react'
-import { Paper, Stack } from '@mui/material'
-import SearchField from '../TextField/SearchField'
-import AddButton from '../Button/AddButton'
+import React from 'react';
+import { Paper, Stack } from '@mui/material';
+import SearchField from '../TextField/SearchField';
+import AddButton from '../Button/AddButton';
+const userInfor = JSON.parse(localStorage.getItem('USERINFOR'));
 
 export const Header = (link) => {
-    return (
-        <Paper sx={{ width: '100%', mb: 2, padding: '20px', boxShadow: 'none'}}>
-            <Stack direction='row' justifyContent='space-between'>
-                {AddButton(link)}
-                <SearchField />
-            </Stack>
-        </Paper>
-    )
-}
+  return (
+    <Paper sx={{ width: '100%', mb: 2, padding: '20px', boxShadow: 'none' }}>
+      <Stack direction="row" justifyContent="space-between">
+        {userInfor.authorID === '54' || userInfor.authorID === '24'
+          ? AddButton(link)
+          : null}
+        <SearchField />
+      </Stack>
+    </Paper>
+  );
+};
 
-export default Header
+export default Header;
