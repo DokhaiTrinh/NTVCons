50900d3dfe0ae49309bf1a71ccabed7f1776c8c3
Hassan97 <nguyenddse62732@gmail.com>
2022-09-13 11:33:53 +0700

    commit

--- src/apis/Message/getConversationById.js
+++ src/apis/Message/getConversationById.js
@@ -1,8 +1,6 @@
 import axiosService from '../../axios/axiosService';
 import * as API_LINK from './../../contants/ApiLinks/apiLinks';
 export const getConversationsById = (
-  ipAddress,
-  searchType,
   conversationId,
   pageNo,
   pageSize,
@@ -11,7 +9,7 @@ export const getConversationsById = (
 ) => {
   const userInfor = JSON.parse(localStorage.getItem('USERINFOR'));
   return axiosService.get(
-    `${API_LINK.GET_BY_CONVERSATION_ID}?ipAddress=${ipAddress}&searchType=${searchType}&conversationId=${conversationId}&pageNo=${pageNo}&pageSize=${pageSize}&sortBy=${sortBy}&sortTypeAsc=${sortTypeAsc}`,
+    `${API_LINK.GET_BY_CONVERSATION_ID}?conversationId=${conversationId}&pageNo=${pageNo}&pageSize=${pageSize}&sortBy=${sortBy}&sortTypeAsc=${sortTypeAsc}`,
     userInfor.token
   );
 };
